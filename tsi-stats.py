8901ef8d7c4d19be071a890eb2256a805c813905
VJ Kapur <vj@vjkapur.com>
2025-01-24 09:43:25 -0500

    updates to make scripts work in 2025

--- tsi-stats.py
+++ tsi-stats.py
@@ -8,7 +8,7 @@ print('reading in data files; could take a while')
 
 # read in GeoJSON files (reduce years for testing because the whole decade takes a while)
 datasets = [gpd.read_file('data/311_City_Service_Requests_in_%i.geojson' % year)
-            for year in range(2013, 2024)]
+            for year in range(2013, 2026)]
 
 print('files read in')
 
@@ -23,8 +23,9 @@ date_attrs = {'year': 4, 'month': 7, 'date': 10}
 
 # exporting CSVs of counts
 for target_attr in date_attrs:
-    tsi_311_data[target_attr] = tsi_311_data.ADDDATE.str.slice(stop=date_attrs[target_attr])
+    tsi_311_data[target_attr] = tsi_311_data.ADDDATE.astype(str).str.slice(stop=date_attrs[target_attr])
     tsi_311_data[target_attr].value_counts(dropna=False).to_csv('tsi_counts_by_%s.csv'%target_attr, header=True, index=True)
 
 # rolling 7-day count
-tsi_311_data.date.value_counts().sort_index().rolling(7).sum().to_csv('tsi_counts_by_7day.csv', header=True)
+# tsi_311_data.date.value_counts().sort_index().rolling(7).sum().to_csv('tsi_counts_by_7day.csv', header=True)
+tsi_311_data.ADDDATE.astype(str).str.slice(stop=10).value_counts().sort_index().rolling(7).sum().to_csv('tsi_counts_by_7day.csv', header=True)
